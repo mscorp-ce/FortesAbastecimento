3b963e22bebfa2567c1b824c2944ecd1e645527c
Marcio.Silva <m.s.copr@gmail.com>
2025-12-12 14:59:33 -0300

    :sparkles: Adicionado ao projeto FortesAbastecimento as units
     - uModel.Abstraction
     - uModel.FireDACEngineException
     - uModel.FireDAC.Transaction
     - uInfra.Libary
     - uModel.FireDAC
     - uModel.Repository.DataManager
     - uModel.Design.Factory.DataManagerFactory

--- src/FortesAbastecimento.dpr
+++ src/FortesAbastecimento.dpr
@@ -5,7 +5,14 @@ uses
   uMain in 'view\uMain.pas' {frmMain},
   uModel.Entities.Tanque in 'model\entities\uModel.Entities.Tanque.pas',
   uModel.Entities.Bomba in 'model\entities\uModel.Entities.Bomba.pas',
-  uModel.uModel.Entities.Abastecimento in 'model\entities\uModel.uModel.Entities.Abastecimento.pas';
+  uModel.uModel.Entities.Abastecimento in 'model\entities\uModel.uModel.Entities.Abastecimento.pas',
+  uModel.Abstraction in 'model\uModel.Abstraction.pas',
+  uModel.FireDACEngineException in 'model\uModel.FireDACEngineException.pas',
+  uModel.FireDAC.Transaction in 'model\uModel.FireDAC.Transaction.pas',
+  uInfra.Libary in 'Infra\uInfra.Libary.pas',
+  uModel.FireDAC in 'model\uModel.FireDAC.pas',
+  uModel.Repository.DataManager in 'model\repository\uModel.Repository.DataManager.pas',
+  uModel.Design.Factory.DataManagerFactory in 'model\design\factory\uModel.Design.Factory.DataManagerFactory.pas';
 
 {$R *.res}
 
