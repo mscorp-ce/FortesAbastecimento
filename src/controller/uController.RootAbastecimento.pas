2a62ad8894cf09ad28bbee0d183bca7ee5c36978
Marcio.Silva <m.s.copr@gmail.com>
2025-12-13 16:31:52 -0300

    :sparkles: Adicionado ao projeto a classe "TControllerRootAbastecimento", a mesma implementa  o Controller para Abastecimento (Sera utilizado em consultas)
    - Cria TControllerRootAbastecimento como camada de entrada para Abastecimento
    - Encapsula acesso ao serviço de Abastecimento
    - Expõe operações de consulta e metadados para a camada de apresentação

---
+++ src/controller/uController.RootAbastecimento.pas
@@ -0,0 +1,72 @@
+unit uController.RootAbastecimento;
+
+interface
+
+uses
+  System.Classes, System.Generics.Collections, uModel.Abstraction,
+  uModel.Entities.Abastecimento;
+
+type
+  TControllerRootAbastecimento = class(TInterfacedObject, IRootController<TAbastecimento>)
+  private
+    AbastecimentoService: IService<TAbastecimento>;
+  public
+    function Fields: TStrings;
+    function CommandSQL(): String;
+    function FindAll(): TObjectList<TAbastecimento>; overload;
+    function FindAll(CommadSQL: String): TObjectList<TAbastecimento>; overload;
+    function FindAll(CommadSQL: String; Entity: TAbastecimento): TObjectList<TAbastecimento>; overload;
+
+    constructor Create; reintroduce;
+    destructor Destroy; override;
+  end;
+
+implementation
+
+{ TControllerRootProject }
+
+uses uModel.Service.Abastecimento;
+
+function TControllerRootAbastecimento.CommandSQL: String;
+begin
+  Result := AbastecimentoService.CommandSQL();
+end;
+
+constructor TControllerRootAbastecimento.Create();
+begin
+  inherited Create;
+
+  AbastecimentoService:= TAbastecimentoService.Create();
+end;
+
+destructor TControllerRootAbastecimento.Destroy();
+begin
+  inherited;
+end;
+
+function TControllerRootAbastecimento.Fields(): TStrings;
+var
+  Items: TStrings;
+begin
+  Items:= AbastecimentoService.Fields;
+
+  Result:= Items;
+end;
+
+function TControllerRootAbastecimento.FindAll(): TObjectList<TAbastecimento>;
+begin
+  Result:= nil;
+end;
+
+function TControllerRootAbastecimento.FindAll(CommadSQL: String;
+  Entity: TAbastecimento): TObjectList<TAbastecimento>;
+begin
+  Result:= nil;
+end;
+
+function TControllerRootAbastecimento.FindAll(CommadSQL: String): TObjectList<TAbastecimento>;
+begin
+  Result:= AbastecimentoService.FindAll(CommadSQL);
+end;
+
+end.
