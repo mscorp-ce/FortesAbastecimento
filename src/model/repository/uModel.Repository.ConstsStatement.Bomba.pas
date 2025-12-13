44f28cb4962f7fc75a9f7aaf457666850000324d
Marcio.Silva <m.s.copr@gmail.com>
2025-12-13 16:19:30 -0300

    :sparkles: Adicionado ao projeto as queries de persistência da entidade bombas

---
+++ src/model/repository/uModel.Repository.ConstsStatement.Bomba.pas
@@ -0,0 +1,20 @@
+unit uModel.Repository.ConstsStatement.Bomba;
+
+interface
+
+const
+  QUERY_BOMBA =
+    'SELECT BOM.ID_BOMBA, ' + sLineBreak +
+    '       BOM.ID_TANQUE, ' + sLineBreak +
+    '       BOM.NUMERO_BOMBA, ' + sLineBreak +
+    '       BOM.DESCRICAO ' + sLineBreak +
+    '  FROM BOMBAS BOM ';
+
+  QUERY_CLASE_WHERE_BY_ID = ' WHERE ID_BOMBA = :ID_BOMBA ';
+
+  QUERY_BOMBA_BY_ID =
+    QUERY_BOMBA + QUERY_CLASE_WHERE_BY_ID;
+
+implementation
+
+end.
