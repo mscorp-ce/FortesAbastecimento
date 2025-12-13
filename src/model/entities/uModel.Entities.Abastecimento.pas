e1d898f5a24934b56c0772e6fa5db48ad1bbffe9
Marcio.Silva <m.s.copr@gmail.com>
2025-12-13 15:43:55 -0300

    fix(model): corrige entity Abastecimento e ajusta namespace
    - Corrige o namespace da unit para uModel.Entities.Abastecimento
    - Ajusta estrutura da classe TAbastecimento
    - Inclui gerenciamento de ciclo de vida da entidade Bomba
    - Separa preço por litro do valor total
    - Centraliza cálculo de imposto via getter

---
+++ src/model/entities/uModel.Entities.Abastecimento.pas
@@ -0,0 +1,58 @@
+unit uModel.Entities.Abastecimento;
+
+interface
+
+uses
+  System.SysUtils, System.Generics.Collections, uModel.Entities.Bomba;
+
+type
+  TAbastecimento = class
+  private
+    FId: Integer;
+    FBomba: TBomba;
+    FDataHora: TDateTime;
+    FQuantidadeLitros: Double;
+    FPrecoLitro: Currency;
+    FValorTotal: Currency;
+    FImposto: Currency;
+    function GetImposto: Currency;
+  public
+    constructor Create();
+    destructor Destroy(); override;
+
+    property Id: Integer read FId write FId;
+    property Bomba: TBomba read FBomba write FBomba;
+    property DataHora: TDateTime read FDataHora write FDataHora;
+    property QuantidadeLitros: Double read FQuantidadeLitros write FQuantidadeLitros;
+    property PrecoLitro: Currency read FPrecoLitro write FPrecoLitro;
+    property ValorTotal: Currency read FValorTotal write FValorTotal;
+    property Imposto: Currency read GetImposto write FImposto;
+  end;
+
+implementation
+
+{ TAbastecimento }
+
+uses
+  System.Math;
+
+constructor TAbastecimento.Create();
+begin
+  inherited Create();
+
+  FBomba := TBomba.Create();
+end;
+
+destructor TAbastecimento.Destroy();
+begin
+  FBomba.Free();
+
+  inherited Destroy();
+end;
+
+function TAbastecimento.GetImposto: Currency;
+begin
+  Result := RoundTo(FValorTotal * 0.13, -2);
+end;
+
+end.
