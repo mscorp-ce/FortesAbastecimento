a355c80d89f87bc2b0d489d5b7cffc76ac1ea443
Marcio.Silva <m.s.copr@gmail.com>
2025-12-13 16:41:13 -0300

    :sparkles: Modificado o projeto "FortesAbastecimento", integrando camadas de repositório, service, controller e filtros ao projeto
    - Corrige referência da unit uModel.Entities.Abastecimento no DPR
    - Registra módulos de Abastecimento nas camadas Repository, Service e Controller
    - Adiciona infraestrutura de Statement e Factory para repositórios
    - Inclui exceção de regra de negócio para Abastecimento
    - Integra formulário customizado de filtros de Abastecimento
    - Remove criação automática de formulários do auto create do projeto

--- src/FortesAbastecimento.dpr
+++ src/FortesAbastecimento.dpr
@@ -5,7 +5,7 @@ uses
   uMain in 'view\uMain.pas' {frmMain},
   uModel.Entities.Tanque in 'model\entities\uModel.Entities.Tanque.pas',
   uModel.Entities.Bomba in 'model\entities\uModel.Entities.Bomba.pas',
-  uModel.uModel.Entities.Abastecimento in 'model\entities\uModel.uModel.Entities.Abastecimento.pas',
+  uModel.Entities.Abastecimento in 'model\entities\uModel.Entities.Abastecimento.pas',
   uModel.Abstraction in 'model\uModel.Abstraction.pas',
   uModel.FireDACEngineException in 'model\uModel.FireDACEngineException.pas',
   uModel.FireDAC.Transaction in 'model\uModel.FireDAC.Transaction.pas',
@@ -15,7 +15,19 @@ uses
   uModel.Design.Factory.DataManagerFactory in 'model\design\factory\uModel.Design.Factory.DataManagerFactory.pas',
   uView.FormConsult in 'view\consult\uView.FormConsult.pas' {frmConsult},
   uView.FormBaseRegistration in 'view\form_registration\uView.FormBaseRegistration.pas' {frmBaseRegistration},
-  uView.FormFilters in 'view\consult\uView.FormFilters.pas' {frmFiltros};
+  uView.FormFilters in 'view\consult\uView.FormFilters.pas' {frmFiltros},
+  uView.CustomFormFilterAbastecimento in 'view\consult\uView.CustomFormFilterAbastecimento.pas',
+  uModel.Repository.Statement in 'model\repository\uModel.Repository.Statement.pas',
+  uModel.Repository.StatementFactory in 'model\repository\design\uModel.Repository.StatementFactory.pas',
+  uModel.Repository.DataManagerFactory in 'model\repository\design\uModel.Repository.DataManagerFactory.pas',
+  uModel.Repository.ConstsStatement.Abastecimento in 'model\repository\uModel.Repository.ConstsStatement.Abastecimento.pas',
+  uModel.Repository.Abastecimento in 'model\repository\uModel.Repository.Abastecimento.pas',
+  uModel.Service.Abastecimento in 'model\services\uModel.Service.Abastecimento.pas',
+  uController.RootAbastecimento in 'controller\uController.RootAbastecimento.pas',
+  uModel.Services.Exception.EServiceAbastecimentoBusinessException in 'model\services\uModel.Services.Exception.EServiceAbastecimentoBusinessException.pas',
+  uController.Abastecimento in 'controller\uController.Abastecimento.pas',
+  uModel.Repository.Bomba in 'model\repository\uModel.Repository.Bomba.pas',
+  uModel.Repository.ConstsStatement.Bomba in 'model\repository\uModel.Repository.ConstsStatement.Bomba.pas';
 
 {$R *.res}
 
@@ -24,8 +36,5 @@ begin
   Application.MainFormOnTaskbar := True;
   ReportMemoryLeaksOnShutdown := True;
   Application.CreateForm(TfrmMain, frmMain);
-  Application.CreateForm(TfrmConsult, frmConsult);
-  Application.CreateForm(TfrmBaseRegistration, frmBaseRegistration);
-  Application.CreateForm(TfrmFiltros, frmFiltros);
   Application.Run;
 end.
