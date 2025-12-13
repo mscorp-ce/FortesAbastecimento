1703bc5a01be03e572d19ed7b13c41614ca83954
Marcio.Silva <m.s.copr@gmail.com>
2025-12-13 16:33:22 -0300

    :sparkles: Adicionado ao projeto a classe "TControllerAbastecimento", a mesma implementa  o Controller para Abastecimento
    - Cria TControllerAbastecimento como camada de controle do domínio
    - Encapsula chamadas ao AbastecimentoService
    - Expõe operações de validação, consulta, inclusão, alteração e exclusão
    - Mantém separação entre camada de apresentação e regras de negócio

---
+++ src/controller/uController.Abastecimento.pas
@@ -0,0 +1,106 @@
+unit uController.Abastecimento;
+
+interface
+
+uses
+  System.Classes, System.Generics.Collections, uModel.Abstraction, uModel.Entities.Abastecimento;
+
+type
+  TControllerAbastecimento = class(TInterfacedObject, IController<TAbastecimento>)
+  private
+    AbastecimentoService: IService<TAbastecimento>;
+  public
+    function Fields(): TStrings;
+    function CommandSQL(): String;
+    function FindAll(): TObjectList<TAbastecimento>; overload;
+
+    function IsValid(Entity: TAbastecimento; out MessageContext: String): Boolean;
+    function Save(Entity: TAbastecimento): Boolean;
+    function Update(Entity: TAbastecimento): Boolean;
+    function DeleteById(Entity: TAbastecimento): Boolean;
+    function Find(): Integer;
+    function FindById(Id: Integer): TAbastecimento;
+    function FindAll(CommandSQL: String): TObjectList<TAbastecimento>; overload;
+
+    constructor Create(); reintroduce;
+    destructor Destroy(); override;
+  end;
+
+implementation
+
+{ TControllerAbastecimento }
+
+uses
+  uModel.Service.Abastecimento;
+
+function TControllerAbastecimento.CommandSQL(): string;
+begin
+  Result := AbastecimentoService.CommandSQL();
+end;
+
+constructor TControllerAbastecimento.Create();
+begin
+  inherited Create();
+
+  AbastecimentoService := TAbastecimentoService.Create();
+end;
+
+function TControllerAbastecimento.DeleteById(Entity: TAbastecimento): Boolean;
+begin
+  Result := AbastecimentoService.DeleteById(Entity);
+end;
+
+destructor TControllerAbastecimento.Destroy();
+begin
+  inherited Destroy();
+end;
+
+function TControllerAbastecimento.Fields: TStrings;
+var
+  Items: TStrings;
+begin
+  Items:= AbastecimentoService.Fields;
+
+  Result:= Items;
+end;
+
+function TControllerAbastecimento.Find(): Integer;
+begin
+  Result:= AbastecimentoService.Find();
+end;
+
+function TControllerAbastecimento.FindAll: TObjectList<TAbastecimento>;
+begin
+  Result:= AbastecimentoService.FindAll();
+end;
+
+function TControllerAbastecimento.FindAll(CommandSQL: String): TObjectList<TAbastecimento>;
+begin
+  Result:= AbastecimentoService.FindAll(CommandSQL);
+end;
+
+function TControllerAbastecimento.FindById(Id: Integer): TAbastecimento;
+begin
+  Result:= AbastecimentoService.FindById(Id);
+end;
+
+function TControllerAbastecimento.IsValid(Entity: TAbastecimento; out MessageContext: String): Boolean;
+begin
+  try
+    Result := AbastecimentoService.IsValid(Entity, MessageContext);
+  except
+    raise;
+  end;
+end;
+
+function TControllerAbastecimento.Save(Entity: TAbastecimento): Boolean;
+begin
+  Result := AbastecimentoService.Save(Entity);
+end;
+
+function TControllerAbastecimento.Update(Entity: TAbastecimento): Boolean;
+begin
+  Result:= AbastecimentoService.Update(Entity);
+end;
+
+end.
